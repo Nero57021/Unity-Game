3a906c42d53ec40b43d142788e9562577feb8e66
Jeremy B <Jeremy57021@users.noreply.github.com>
2020-10-17 14:14:47 -0500

    Added character animation, checks for triggers and reactive switching

--- Introductory Project/Assets/Scripts/movement.cs
+++ Introductory Project/Assets/Scripts/movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    public Animator anim;
     public float movementSpeed;
     public Rigidbody2D rb;
     public float jumpForce = 20f;
@@ -19,6 +20,23 @@ public class movement : MonoBehaviour
         {
             Jump();
         }
+        if(Mathf.Abs(mx)> 0.05f)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+        if(mx > 0f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if(mx < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        anim.SetBool("isGrounded", isGrounded());
     }
 
     private void Jump()
